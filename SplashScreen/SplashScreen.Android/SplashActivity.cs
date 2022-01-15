3c5a5fbb4e36b8fc054f146ef01b65e295f533e2
theokunb <makarovalexeyvery@gmail.com>
2022-01-15 18:40:56 +0900

    Добавьте файлы проекта.

---
+++ SplashScreen/SplashScreen.Android/SplashActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SplashScreen.Droid
+{
+    [Activity(Label = "theokunb", Icon = "@mipmap/icon", NoHistory =false, Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity:Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
---
+++ SplashScreen/SplashScreen/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SplashScreen
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            await Task.WhenAll(
+                SplashGrid.FadeTo(0, 2000),
+                icon.ScaleTo(10, 2000)
+                );
+        }
+    }
+}
